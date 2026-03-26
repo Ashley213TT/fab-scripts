1d09a9d73d39ebad5a4c629ce35c50a2e2faefdd
Ashley213TT <151547185+Ashley213TT@users.noreply.github.com>
2026-03-26 15:34:43 -0600

    matt mi_script.py

--- mi_script.py
+++ mi_script.py
@@ -3,3 +3,11 @@ print('Corrección de formato')
 print('Ejemplo de uso')
 print('Corrección de formato')
 print('Ejemplo de uso')
+
+# Este script multiplica dos números ingresados por el usuario
+
+print("El resultado de 2 x 2 es:", 2 * 2)
+
+a = int(input("Ingresa un número: "))
+b = int(input("Ingresa otro número: "))
+print("El resultado es:", a * b)
